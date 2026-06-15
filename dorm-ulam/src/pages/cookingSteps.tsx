4d1b59a707e774d0f18100c07b4396ee959d5e24
Stephanie Natividad <natividadstephaniem@gmail.com>
2026-06-15 19:06:05 +0800

    fixed subtitle

--- dorm-ulam/src/pages/cookingSteps.tsx
+++ dorm-ulam/src/pages/cookingSteps.tsx
@@ -82,7 +82,8 @@ export default function CookingSteps({ recipe, token, onBack, onDone }: Props) {
             <p className="congrats-emoji">🎉</p>
             <h1 className="congrats-title">Luto na!</h1>
             <p className="congrats-subtitle">
-              You just cooked <span className="congrats-highlight">{recipe.name}</span>. Eatwell, Lab!
+              You just cooked <span className="congrats-highlight">{recipe.name}</span>. 
+              Eatwell, Lab!
             </p>
           </div>
 
